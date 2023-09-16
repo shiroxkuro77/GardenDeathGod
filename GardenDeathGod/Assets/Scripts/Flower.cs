28953dfe9608d3c928503598ad2c3a2687729919
Li Haoquan <64543157+lihaoquan@users.noreply.github.com>
2023-09-16 21:21:41 +0800

    Flower patch

---
+++ GardenDeathGod/Assets/Scripts/Flower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : EntityUnit
+{
+    public override void ExecuteBehaviour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+}
